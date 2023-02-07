a77221d1551edbcb09dcc06bb2f4b1c3987f9e63
Lemon Soju <yckk980320@naver.com>
2023-02-07 14:46:03 +0900

    feat(controller): add user join Controller

--- src/main/java/com/lemonSoju/blog/dto/request/UserJoinRequestDto.java
+++ src/main/java/com/lemonSoju/blog/dto/request/UserJoinRequestDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import javax.validation.constraints.NotEmpty;
 
 @Data
-public class UserJoinDtoRequest {
+public class UserJoinRequestDto {
 
     @NotEmpty(message = "회원 id는 필수입니다.")
     private String uid;
@@ -16,7 +16,7 @@ public class UserJoinDtoRequest {
     private String name;
 
     @Builder
-    public UserJoinDtoRequest(String uid, String pwd, String name) {
+    public UserJoinRequestDto(String uid, String pwd, String name) {
         this.uid = uid;
         this.pwd = pwd;
         this.name = name;
