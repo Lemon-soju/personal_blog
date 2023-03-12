439afc89d2486622540623ad7140f0ebdc398970
Lemon Soju <yckk980320@naver.com>
2023-03-12 10:26:46 +0900

    feat: add signup api specification

--- src/main/java/com/lemonSoju/blog/config/WebMvcConfig.java
+++ src/main/java/com/lemonSoju/blog/config/WebMvcConfig.java
@@ -33,6 +33,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthInterceptor(jwtService))
-                .excludePathPatterns("/error, /favicon.ico", "/signup", "/login", "/post/*", "/post");
+                .excludePathPatterns("/error, /favicon.ico", "/signup", "/login", "/post/*", "/post", "/docs/index.html");
     }
 }
