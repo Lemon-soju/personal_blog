7a09856dbb4fefa665f5be25431d01fef0f69b6c
Lemon Soju <yckk980320@naver.com>
2023-09-06 16:47:53 +0900

    mod: remove heart table and change userId to memberId

--- src/main/java/com/lemonSoju/blog/domain/Member.java
+++ src/main/java/com/lemonSoju/blog/domain/Member.java
@@ -1,10 +1,8 @@
 package com.lemonSoju.blog.domain;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -13,13 +11,11 @@ import java.util.List;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class Member {
     @Id
     @GeneratedValue
-    @Column(name = "user_id")
+    @Column(name = "member_id")
     private Long id;
     private String uid;
     private String pwd;
@@ -29,15 +25,14 @@ public class Member {
     @OneToMany(mappedBy = "writer")
     private List<Post> posts = new ArrayList<>();
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "post_id")
-    private Post heart;
-
     @Builder
     public Member(String uid, String pwd, String name, String authority) {
         this.uid = uid;
         this.pwd = pwd;
         this.name = name;
         this.authority = authority;
+    }
+
+    protected Member() {
     }
 }
