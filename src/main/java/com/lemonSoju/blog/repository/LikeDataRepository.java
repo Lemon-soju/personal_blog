8411670981c3c663acc0c4f6a1e82b43758aee7f
Lemon Soju <yckk980320@naver.com>
2023-09-24 14:52:43 +0900

    ftr: add findByPostIdAndMemberId to repository

--- src/main/java/com/lemonSoju/blog/repository/LikeDataRepository.java
+++ src/main/java/com/lemonSoju/blog/repository/LikeDataRepository.java
@@ -4,5 +4,8 @@ package com.lemonSoju.blog.repository;
 import com.lemonSoju.blog.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeDataRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByPostIdAndMemberId(Long postId, Long memberId);
 }
