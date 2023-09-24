096426f829c4e16c482d20754577aaae23d9824f
Lemon Soju <yckk980320@naver.com>
2023-09-24 15:17:31 +0900

    fix: resolve duplicate like create problem

--- src/main/java/com/lemonSoju/blog/controller/LikeController.java
+++ src/main/java/com/lemonSoju/blog/controller/LikeController.java
@@ -1,6 +1,6 @@
 package com.lemonSoju.blog.controller;
 
-import com.lemonSoju.blog.service.MemberService;
+import com.lemonSoju.blog.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -14,15 +14,15 @@ import static com.lemonSoju.blog.service.JwtService.ACCESS_TOKEN;
 @RequiredArgsConstructor
 public class LikeController {
 
-    private final MemberService memberService;
+    private final LikeService likeService;
 
     @PostMapping("like/post/{postId}")
     public ResponseEntity createLike(@PathVariable Long postId, @RequestHeader HttpHeaders headers) {
-        return memberService.createLike(postId, headers.getFirst(ACCESS_TOKEN));
+        return likeService.createLike(postId, headers.getFirst(ACCESS_TOKEN));
     }
 
     @DeleteMapping("like/post/{postId}")
     public ResponseEntity deleteLike(@PathVariable Long postId, @RequestHeader HttpHeaders headers) {
-        return memberService.deleteLike(postId, headers.getFirst(ACCESS_TOKEN));
+        return likeService.deleteLike(postId, headers.getFirst(ACCESS_TOKEN));
     }
 }
