f6abb30958c2dafdb55e9a9d7dd859b99613635c
Lemon Soju <yckk980320@naver.com>
2023-10-08 20:16:21 +0900

    ftr: add count func to post repository

--- src/main/java/com/lemonSoju/blog/repository/PostDataRepository.java
+++ src/main/java/com/lemonSoju/blog/repository/PostDataRepository.java
@@ -1,6 +1,7 @@
 package com.lemonSoju.blog.repository;
 
 import com.lemonSoju.blog.domain.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,11 @@ import java.util.List;
 public interface PostDataRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByTitleContaining(String search);
 
-    @Query("SELECT p FROM Post p JOIN FETCH p.writer WHERE :search IS NULL OR p.title LIKE %:search%")
-    List<Post> findAllWithFetchJoin(String search);
+    @Query("SELECT p FROM Post p JOIN FETCH p.writer WHERE (:search IS NULL OR p.title LIKE %:search%) AND (:writer IS NULL OR p.writer.uid = :writer)")
+    List<Post> findAllWithFetchJoin(String search, String writer, Pageable pageable);
+
+    long count();
+
+    @Query("SELECT COUNT(p) FROM Post p WHERE :search IS NULL OR p.title LIKE %:search%")
+    long countBySearch(String search);
 }
